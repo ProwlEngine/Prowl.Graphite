c4916a84a194349cfdbea67737e328df551b52f1
Kai Angulo <kangriebling@gmail.com>
2025-08-15 23:48:07 -0700

    Fix program and create SDL window

--- Samples/HelloWorld/Program.cs
+++ Samples/HelloWorld/Program.cs
@@ -5,57 +5,62 @@ using Silk.NET.SDL;
 using Prowl.Graphite;
 using Prowl.Graphite.OpenGL;
 using Prowl.Vector;
+using System.Collections.Generic;
 
 
 public unsafe class Program
 {
-    Sdl sdl;
-    Window* window;
-    SdlContext context;
+    static Sdl sdl;
+    static Window* window;
+    static SdlContext context;
 
     const string ShaderSource = """
 
 
 """;
 
-    GraphicsDevice device;
+    static GraphicsDevice device;
 
-    Shader shader;
-    Mesh mesh;
-    Material material;
-    CommandBuffer buffer;
-    RenderTexture target;
+    static Shader shader;
+    static Mesh mesh;
+    static Material material;
+    static CommandBuffer buffer;
+    static RenderTexture target;
 
 
-    public void SetupWindow()
+    public static void SetupWindow()
     {
         sdl = Sdl.GetApi();
         window = sdl.CreateWindow("My Window", 0, 0, 500, 500, (uint)WindowFlags.Opengl);
         context = new SdlContext(sdl, window);
+        context.Create();
+        context.MakeCurrent();
     }
 
 
-    public void Main()
+    public static void Main()
     {
         SetupWindow();
 
         device = new GLGraphicsDevice(context);
 
         if (!ShaderCompiler.CompileShader(ShaderSource, out shader))
-            return;
+            Console.WriteLine("Failed to compile shader");
 
-        mesh = new Mesh();
+        mesh = Mesh.Create();
 
         mesh.SetVertices(new List<Vector3>());
         mesh.SetIndices(new List<int>());
 
-        material = new Material(shader);
-        target = new RenderTexture(1960, 1080, RenderTextureFormat.RGBA32);
+        material = Material.Create(shader);
+        target = RenderTexture.Create(1960, 1080, RenderTextureFormat.RGBA32);
 
-        buffer = new CommandBuffer("Main Buffer");
+        buffer = CommandBuffer.Create("Main Buffer");
 
         while (true)
         {
+            Console.WriteLine("Polling");
+
             Event sdlEvent = default;
             while (sdl.PollEvent(ref sdlEvent) != 0)
             {
@@ -66,16 +71,21 @@ public unsafe class Program
             buffer.Clear();
 
             buffer.SetRenderTarget(target);
-            buffer.ClearRenderTarget(Vector4.zero, 1.0, 0);
+            buffer.ClearRenderTarget(Vector4.one, 1.0, 0);
 
-            material.SetVector("Color", new Vector3(0.5, 0.5, 1.0));
-            material.SetMatrix("MVP", Matrix4x4.Identity);
+            // material.SetVector("Color", new Vector3(0.5, 0.5, 1.0));
+            // material.SetMatrix("MVP", Matrix4x4.Identity);
 
             buffer.SetMaterial(material, 0);
 
             buffer.DrawMesh(mesh, material);
 
             device.SubmitCommands(buffer);
+
+            while (!device.IsIdle)
+                System.Threading.Thread.Sleep(1);
+
+            Console.WriteLine(device.IsIdle);
 
             device.SwapBuffers();
         }

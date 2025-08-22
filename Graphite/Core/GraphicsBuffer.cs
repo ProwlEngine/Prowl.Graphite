434143ffe1356519921851ed6c8be676224bf972
Kai Angulo <kangriebling@gmail.com>
2025-08-22 00:26:56 -0700

    Improve GraphicsBuffer API

--- Graphite/Core/GraphicsBuffer.cs
+++ Graphite/Core/GraphicsBuffer.cs
@@ -6,25 +6,50 @@ using Prowl.Vector;
 namespace Prowl.Graphite;
 
 
-public abstract class GraphicsBuffer : IDisposable
+public abstract unsafe class GraphicsBuffer : IDisposable
 {
-    public static GraphicsBuffer Create()
+    public static GraphicsBuffer Create(GraphicsBufferCreateInfo info, GraphicsDevice? device = null)
     {
+        device ??= GraphicsDevice.Instance;
+
         return GraphicsDevice.Backend switch
         {
-            GraphicsBackend.OpenGL => new OpenGL.GLGraphicsBuffer()
+            GraphicsBackend.OpenGL => new OpenGL.GLGraphicsBuffer(info, (OpenGL.GLGraphicsDevice)device)
         };
     }
 
-    public int Count { get; private set; }
+    public abstract bool IsValid { get; }
 
-    public string Name { get; set; }
+    public abstract MapState MapState { get; }
 
-    public int Stride { get; private set; }
+    public abstract int Count { get; }
+
+    public abstract int Stride { get; }
 
     public int Size => Count * Stride;
 
-    public BufferUsage Usage { get; private set; }
+
+    public abstract BufferTarget Target { get; }
+    public abstract BufferUsage Usage { get; }
+
 
     public abstract void Dispose();
+
+    public abstract void GetData(void* data, int destinationIndex, int sourceIndex, int countBytes);
+
+    public abstract void SetData(void* data, int sourceIndex, int destinationIndex, int countBytes);
+
+    public abstract void* MapBuffer();
+
+    public abstract void UnmapBuffer();
+}
+
+
+public struct GraphicsBufferCreateInfo
+{
+    public BufferTarget Target;
+    public BufferUsage Usage;
+
+    public int Count;
+    public int Stride;
 }

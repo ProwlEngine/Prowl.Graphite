909e12b9aa08d6b0b970a2523247a250584dcc75
Kai Angulo <kangriebling@gmail.com>
2025-08-29 02:15:53 -0700

    Finalize Mesh API and continue implementing GLMesh

--- Graphite/Core/Mesh.cs
+++ Graphite/Core/Mesh.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 using Prowl.Vector;
@@ -8,17 +9,47 @@ namespace Prowl.Graphite;
 
 public abstract class Mesh
 {
-    public static Mesh Create(GraphicsDevice? device = null)
+    public static Mesh Create(MeshCreateInfo? createInfo = null, GraphicsDevice? device = null)
     {
+        createInfo ??= new MeshCreateInfo();
         device ??= GraphicsDevice.Instance;
 
         return GraphicsDevice.Backend switch
         {
-            GraphicsBackend.OpenGL => new OpenGL.GLMesh()
+            GraphicsBackend.OpenGL => new OpenGL.GLMesh(createInfo.Value, (OpenGL.GLGraphicsDevice)device)
         };
     }
 
+    public abstract bool IsReadable { get; set; }
 
-    public abstract void SetVertices(List<Float3> vertices);
-    public abstract void SetIndices(List<int> indices);
+    public abstract void SetVertexInput<T>(Span<T> buffer, int stream) where T : unmanaged;
+    public abstract T[] GetVertexInput<T>(int stream) where T : unmanaged;
+
+    public abstract bool Has32BitIndices { get; }
+
+    public abstract void SetIndexInput32(Span<uint> buffer);
+    public abstract void SetIndexInput16(Span<ushort> buffer);
+
+    public abstract uint[] GetIndexInput32();
+    public abstract ushort[] GetIndexInput16();
+
+    public abstract void SetInputLayout(IEnumerable<VertexInputDescriptor> layout);
+}
+
+
+public struct MeshCreateInfo
+{
+    public static MeshCreateInfo Default = new MeshCreateInfo()
+    {
+        VertexLayout = [
+            new VertexInputDescriptor("POSITION", VertexInputFormat.Float3),
+            new VertexInputDescriptor("NORMAL", VertexInputFormat.Float3),
+            new VertexInputDescriptor("TANGENT", VertexInputFormat.Float3),
+            new VertexInputDescriptor("UV0", VertexInputFormat.Float4),
+            new VertexInputDescriptor("UV1", VertexInputFormat.Float4),
+            new VertexInputDescriptor("UV2", VertexInputFormat.Float4),
+        ]
+    };
+
+    public VertexInputDescriptor[] VertexLayout;
 }
--- Graphite/Platform/OpenGL/GLMesh.cs
+++ Graphite/Platform/OpenGL/GLMesh.cs
@@ -1,18 +1,192 @@
+using System;
 using System.Collections.Generic;
 
-using Prowl.Vector;
+using Silk.NET.OpenGL;
 
 namespace Prowl.Graphite.OpenGL;
 
 
-public class GLMesh : Mesh
+public unsafe class GLMesh : Mesh, GLDeferredResource
 {
-    public override void SetIndices(List<int> indices)
+    private GLGraphicsDevice _device;
+    private VertexArray _vertexArray;
+
+    private VertexInputDescriptor[] _inputLayout;
+
+    private GLGraphicsBuffer[] _vertexInputBuffers;
+
+    private Array?[] _vertexInputArrays;
+
+    internal Array?[] VertexInputArrays
+    {
+        get
+        {
+            _vertexInputArrays ??= new Array?[_inputLayout.Length];
+
+            if (_vertexInputArrays.Length != _inputLayout.Length)
+                Array.Resize(ref _vertexInputArrays, _inputLayout.Length);
+
+            return _vertexInputArrays;
+        }
+    }
+
+    private bool _has32BitIndices;
+
+    private GLGraphicsBuffer _indexBuffer;
+    private Array? _indexArray;
+
+
+    private bool _modifiedBuffers;
+    private bool _modifiedLayout;
+
+
+    public bool Created { get; private set; }
+
+    public override bool IsReadable { get; set; }
+
+    public override bool Has32BitIndices => _has32BitIndices;
+
+    internal int VertexCount => VertexInputArrays[0] != null ? VertexInputArrays[0]!.Length : _vertexInputBuffers[0].Count;
+
+
+
+    public GLMesh(MeshCreateInfo info, GLGraphicsDevice device)
+    {
+        _device = device;
+        SetInputLayout(info.VertexLayout);
+    }
+
+
+
+
+    public override void SetVertexInput<T>(Span<T> buffer, int stream)
     {
 
     }
 
-    public override void SetVertices(List<Float3> vertices)
+
+    public override T[] GetVertexInput<T>(int stream)
+    {
+        if (!IsReadable)
+            throw new Exception("Cannot read from write-only mesh");
+
+        if (VertexInputArrays[stream] == null)
+        {
+            if (_vertexInputBuffers[stream] == null)
+                return [];
+
+            T[] newData = new T[_vertexInputBuffers[stream].Count];
+            _vertexInputBuffers[stream].GetData<T>(newData, 0);
+            VertexInputArrays[stream] = newData;
+        }
+
+        if (VertexInputArrays[stream] is T[] array)
+            return array;
+
+        Type? elementType = VertexInputArrays[stream]!.GetType().GetElementType();
+        throw new Exception($"Attempted to read mesh vertex data stored as {elementType} as {typeof(T)}");
+    }
+
+
+    public override void SetIndexInput32(Span<uint> buffer)
+    {
+        _has32BitIndices = true;
+
+        uint[] indices = new uint[buffer.Length];
+        buffer.CopyTo(indices);
+        _indexArray = indices;
+    }
+
+
+    public override void SetIndexInput16(Span<ushort> buffer)
+    {
+        _has32BitIndices = false;
+
+        ushort[] indices = new ushort[buffer.Length];
+        buffer.CopyTo(indices);
+        _indexArray = indices;
+    }
+
+
+    public override uint[] GetIndexInput32()
+    {
+        if (!IsReadable)
+            throw new Exception("Cannot read from write-only mesh");
+
+        if (!Has32BitIndices)
+            throw new Exception("Mesh contains 16-bit indices. Cannot read 32-bit indices");
+
+        if (_indexArray == null)
+        {
+            if (_indexBuffer == null)
+                return [];
+
+            uint[] newData = new uint[_indexBuffer.Count];
+            _indexBuffer.GetData<uint>(newData, 0);
+            _indexArray = newData;
+        }
+
+        return (_indexArray as uint[])!;
+    }
+
+
+    public override ushort[] GetIndexInput16()
+    {
+        if (!IsReadable)
+            throw new Exception("Cannot read from write-only mesh");
+
+        if (Has32BitIndices)
+            throw new Exception("Mesh contains 32-bit indices. Cannot read 16-bit indices");
+
+        if (_indexArray == null)
+        {
+            if (_indexBuffer == null)
+                return [];
+
+            ushort[] newData = new ushort[_indexBuffer.Count];
+            _indexBuffer.GetData<ushort>(newData, 0);
+            _indexArray = newData;
+        }
+
+        return (_indexArray as ushort[])!;
+    }
+
+
+    public override void SetInputLayout(IEnumerable<VertexInputDescriptor> layout)
+    {
+        _inputLayout = [.. layout];
+
+        if (_inputLayout.Length == 0)
+            _inputLayout = [new VertexInputDescriptor("", VertexInputFormat.Float1)];
+
+        _modifiedLayout = true;
+    }
+
+
+    public unsafe void CreateResource(GL gl)
+    {
+        UpdateMeshData(gl);
+    }
+
+
+    public void UpdateMeshData(GL gl)
+    {
+        if (_modifiedBuffers)
+        {
+
+        }
+
+        if (!IsReadable)
+        {
+            _indexArray = null;
+
+            for (int i = 0; i < VertexInputArrays.Length; i++)
+                VertexInputArrays[i] = null;
+        }
+    }
+
+
+    public void DestroyResource(GL gl)
     {
 
     }

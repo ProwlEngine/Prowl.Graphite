eeefb65f9a82227d0e3ee1764432ef1bf69a8e1a
Kai Angulo <kangriebling@gmail.com>
2026-04-15 18:09:15 -0700

    Add interned semantic ID to vertex input descriptor

--- Graphite/Core/Shader/VertexInputDescriptor.cs
+++ Graphite/Core/Shader/VertexInputDescriptor.cs
@@ -2,12 +2,19 @@
 // Licensed under the MIT License. See the LICENSE file in the project root for details.
 
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace Prowl.Graphite;
 
 
 public readonly record struct VertexInputDescriptor(string semantic, VertexInputFormat format) : IEquatable<VertexInputDescriptor>
 {
+    private static Interner<string, uint> s_semanticInterner = new((x) => x + 1);
+
     public readonly string Semantic = semantic;
+    public readonly uint SemanticID = s_semanticInterner.GetInternedValue(semantic);
     public readonly VertexInputFormat Format = format;
 }

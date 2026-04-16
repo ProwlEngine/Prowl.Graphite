0e921be38b3fffed2a0fa58b689eb03876ddc1df
Kai Angulo <kangriebling@gmail.com>
2026-04-15 18:09:34 -0700

    Add Size() extension to VertexInputFormat

--- Graphite/Core/Shader/VertexInputFormat.cs
+++ Graphite/Core/Shader/VertexInputFormat.cs
@@ -1,3 +1,6 @@
+using System;
+
+
 namespace Prowl.Graphite;
 
 
@@ -34,4 +37,83 @@ public enum VertexInputFormat : byte
     Half1,
     Half2,
     Half4,
+}
+
+
+
+internal static class FormatExtensions
+{
+    // Can't tell if the sizeof() is more readable or not compared to constants but I like all the red in my IDE and it looks more complicated.
+    public static int Size(this VertexInputFormat format) => format switch
+    {
+        VertexInputFormat.Float1 => sizeof(float),
+        VertexInputFormat.Float2 => sizeof(float) * 2,
+        VertexInputFormat.Float3 => sizeof(float) * 3,
+        VertexInputFormat.Float4 => sizeof(float) * 4,
+        VertexInputFormat.Byte2_Norm => sizeof(byte) * 2,
+        VertexInputFormat.Byte2 => sizeof(byte) * 2,
+        VertexInputFormat.Byte4_Norm => sizeof(byte) * 4,
+        VertexInputFormat.Byte4 => sizeof(byte) * 4,
+        VertexInputFormat.SByte2_Norm => sizeof(sbyte) * 2,
+        VertexInputFormat.SByte2 => sizeof(sbyte) * 2,
+        VertexInputFormat.SByte4_Norm => sizeof(sbyte) * 4,
+        VertexInputFormat.SByte4 => sizeof(sbyte) * 4,
+        VertexInputFormat.UShort2_Norm => sizeof(ushort) * 2,
+        VertexInputFormat.UShort2 => sizeof(ushort) * 2,
+        VertexInputFormat.UShort4_Norm => sizeof(ushort) * 4,
+        VertexInputFormat.UShort4 => sizeof(ushort) * 4,
+        VertexInputFormat.Short2_Norm => sizeof(short) * 2,
+        VertexInputFormat.Short2 => sizeof(short) * 2,
+        VertexInputFormat.Short4_Norm => sizeof(short) * 4,
+        VertexInputFormat.Short4 => sizeof(short) * 4,
+        VertexInputFormat.UInt1 => sizeof(uint),
+        VertexInputFormat.UInt2 => sizeof(uint) * 2,
+        VertexInputFormat.UInt3 => sizeof(uint) * 3,
+        VertexInputFormat.UInt4 => sizeof(uint) * 4,
+        VertexInputFormat.Int1 => sizeof(int),
+        VertexInputFormat.Int2 => sizeof(int) * 2,
+        VertexInputFormat.Int3 => sizeof(int) * 3,
+        VertexInputFormat.Int4 => sizeof(int) * 4,
+        VertexInputFormat.Half1 => 2,
+        VertexInputFormat.Half2 => 2 * 2,
+        VertexInputFormat.Half4 => 2 * 4,
+        _ => throw new Exception("Unknown vertex input format")
+    };
+
+
+    public static int Dimension(this VertexInputFormat format) => format switch
+    {
+        VertexInputFormat.Float1 => 1,
+        VertexInputFormat.Float2 => 2,
+        VertexInputFormat.Float3 => 3,
+        VertexInputFormat.Float4 => 4,
+        VertexInputFormat.Byte2_Norm => 2,
+        VertexInputFormat.Byte2 => 2,
+        VertexInputFormat.Byte4_Norm => 4,
+        VertexInputFormat.Byte4 => 4,
+        VertexInputFormat.SByte2_Norm => 2,
+        VertexInputFormat.SByte2 => 2,
+        VertexInputFormat.SByte4_Norm => 4,
+        VertexInputFormat.SByte4 => 4,
+        VertexInputFormat.UShort2_Norm => 2,
+        VertexInputFormat.UShort2 => 2,
+        VertexInputFormat.UShort4_Norm => 4,
+        VertexInputFormat.UShort4 => 4,
+        VertexInputFormat.Short2_Norm => 2,
+        VertexInputFormat.Short2 => 2,
+        VertexInputFormat.Short4_Norm => 4,
+        VertexInputFormat.Short4 => 4,
+        VertexInputFormat.UInt1 => 1,
+        VertexInputFormat.UInt2 => 2,
+        VertexInputFormat.UInt3 => 3,
+        VertexInputFormat.UInt4 => 4,
+        VertexInputFormat.Int1 => 1,
+        VertexInputFormat.Int2 => 2,
+        VertexInputFormat.Int3 => 3,
+        VertexInputFormat.Int4 => 4,
+        VertexInputFormat.Half1 => 1,
+        VertexInputFormat.Half2 => 2,
+        VertexInputFormat.Half4 => 4,
+        _ => throw new Exception("Unknown vertex input format")
+    };
 }

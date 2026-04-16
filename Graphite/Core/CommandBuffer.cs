162891ad7b3e83b78f9e6200191c6cf28da6738a
Kai Angulo <kangriebling@gmail.com>
2026-04-15 18:06:08 -0700

    Add more commands to CommandBuffer

--- Graphite/Core/CommandBuffer.cs
+++ Graphite/Core/CommandBuffer.cs
@@ -32,15 +32,23 @@ public abstract unsafe class CommandBuffer : IDisposable
 
     public abstract void ClearRenderTarget(Byte4 clearColor, double clearDepth, byte clearStencil);
 
-    public abstract void DrawMesh(Mesh mesh, Material material);
+    public abstract void DrawMesh(Mesh mesh, int baseVertex = 0, int indexOffset = 0);
 
-    public abstract void SetScissorRect(Int4 rect, int viewport = 0);
+    public abstract void DrawMeshIndirect(Mesh mesh, GraphicsBuffer indirectBuffer, int indirectArgsOffset = 0, int baseVertex = 0);
 
-    public abstract void SetScissorRects(Int4[] rect, int viewport = 0);
+    public abstract void DrawMeshInstanced(Mesh mesh, int instanceCount, int baseInstance = 0, int baseVertex = 0, int indexOffset = 0);
+
+    public abstract void SetScissorRect(Int4 rect);
+
+    public abstract void SetScissorRects(Int4[] rect);
 
     public abstract void ClearScissorRect();
 
-    public abstract void SetRenderTarget(RenderTexture? target);
+    public abstract void SetDepthRange(float near, float far);
+
+    public abstract void SetViewport(Int2 position, Int2 size);
+
+    public abstract void SetRenderTarget(RenderTarget? target);
 
     /// <summary>
     /// Sets the material and pass to be used in all subsequent draw calls until <see cref="SetMaterial"/> is called again.
@@ -48,6 +56,8 @@ public abstract unsafe class CommandBuffer : IDisposable
     /// <param name="material"></param>
     /// <param name="pass"></param>
     public abstract void SetMaterial(Material material, int pass);
+
+    public abstract void SetShader(Shader shader, int pass);
 
     public abstract void SetBufferData<T>(GraphicsBuffer buffer, Memory<T> data, int sourceIndex, int destinationIndex, int count) where T : unmanaged;
 

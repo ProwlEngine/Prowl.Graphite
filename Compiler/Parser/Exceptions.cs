2b5ce04fc12ef2f7ccd4219e9f46e7e349cf2ef3
Kai Angulo <kangriebling@gmail.com>
2026-04-19 23:10:44 -0700

    Move exceptions into their own block

---
+++ Compiler/Parser/Exceptions.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Prowl.Vector;
+
+using Superpower;
+using Superpower.Parsers;
+using Superpower.Model;
+using Superpower.Tokenizers;
+
+
+namespace Prowl.Graphite;
+
+
+public static class Exceptions
+{
+    public static ParseException Expected(string expected, string found, Position position) =>
+        new ParseException($"Expected '{expected}' but found '{found}'", position);
+
+
+    public static ParseException ExpectedAny(IEnumerable<string> expected, string found, Position position) =>
+        new ParseException($"Expected any of '{string.Join(", ", expected)}' but got '{found}'", position);
+
+    public static ParseException AdjustException(ParseException ex, Position start)
+    {
+        Position adjusted = new(
+            start.Absolute + ex.ErrorPosition.Absolute,
+            start.Line + ex.ErrorPosition.Line - 1,
+            ex.ErrorPosition.Column
+        );
+
+        return new ParseException(ex.Message, adjusted, ex.InnerException);
+    }
+}

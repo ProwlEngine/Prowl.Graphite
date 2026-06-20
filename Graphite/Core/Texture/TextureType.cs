8d1ad0ef597564c0baf63de4b4b3d74f961387f2
Kai Angulo <kangriebling@gmail.com>
2026-06-20 14:12:22 -0700

    Move Texture2D to be the default TextureType when no type is specified.
    This was a nightmare to debug, but thankfully Graphite is *not* broken!

--- Graphite/Core/Texture/TextureType.cs
+++ Graphite/Core/Texture/TextureType.cs
@@ -6,13 +6,13 @@
 public enum TextureType
 {
     /// <summary>
-    /// A one-dimensional Texture.
-    /// </summary>
-    Texture1D,
-    /// <summary>
     /// A two-dimensional Texture.
     /// </summary>
     Texture2D,
+    /// <summary>
+    /// A one-dimensional Texture.
+    /// </summary>
+    Texture1D,
     /// <summary>
     /// A three-dimensional Texture.
     /// </summary>
